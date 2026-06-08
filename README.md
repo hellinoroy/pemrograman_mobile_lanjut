83f74eaa21edda5ab8ccdf0fa9697af75e074cd6
hellinoroy <marchellinoryo@gmail.com>
2026-06-08 19:11:33 +0700

    W11: Soal 3

--- README.md
+++ README.md
@@ -1,3 +1,8 @@
-# uts
+# Pertemuan 11
 
-Preview ada di demo.mp4
+Praktikum 1
+Soal 2 tidak bisa dilakukan karena response RATE_LIMIT_EXCEEDED.
+
+Soal 3 
+subString digunakan untuk manging potongan string dari string lagin, sedangkan catchError digunakan untuk menangkap error,
+demo_pertemuan_11_praktikum_1_soal_3.
--- lib/main.dart
+++ lib/main.dart
@@ -1,38 +1,81 @@
+import 'dart:async';
 import 'package:flutter/material.dart';
-
-import 'package:go_router/go_router.dart';
-import 'package:uts/pages/homepage.dart';
-import 'package:uts/pages/project.dart';
+import 'package:http/http.dart';
+import 'package:http/http.dart' as http;
 
 void main() {
   runApp(const MyApp());
 }
 
-final _router = GoRouter(
-  initialLocation: '/',
-  routes: [
-    GoRoute(
-      path: '/',
-      builder: (context, state) => const Homepage(),
-    ),
-    GoRoute(
-      path: '/project/:name',
-      builder: (context, state) {
-        final name = state.pathParameters['name']!;
-        return Project(name: name);
-      },
-    ),
-  ],
-);
-
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp.router(
-      routerConfig: _router,
+    return MaterialApp(
+      title: '411221221',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      home: FuturePage(),
     );
   }
 }
+
+class FuturePage extends StatefulWidget {
+  const FuturePage({super.key});
+
+  @override
+  State<FuturePage> createState() => _FuturePageState();
+
+}
+
+class _FuturePageState extends State<FuturePage> {
+  String result = '';
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Back from the Future'),
+      ),
+      body: Center(
+        child: Column(
+          children: [
+            const Spacer(),
+            ElevatedButton(
+              onPressed: () {
+                setState(() {});
+                getData()
+                  .then((value) {
+                    result = value.body.toString().substring(0, 450);
+                    setState(() {});
+                  }).catchError((_) {
+                    result = 'An error occured';
+                    setState(() {});
+                  });
+              }, 
+              child: const Text('GO!')
+            ),
+            const Spacer(),
+            Text(result),
+            const Spacer(),
+            const CircularProgressIndicator(),
+            const Spacer(),
+            
+          ],
+        ),
+      ),
+    );
+  }
+
+  Future<Response> getData() async {
+    const authority = 'www.googleapis.com';
+    const path = '/books/v1/volumes/junbDwAAQBAJ';
+    Uri url = Uri.https(authority, path);
+    return http.get(url);
+  }
+}
+
+
