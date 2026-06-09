68549317a107990f6025a8e6eb4258ee9ccfa754
hellinoroy <marchellinoryo@gmail.com>
2026-06-09 20:42:01 +0700

    W12: Jawaban Soal 5

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
-# uts
+Soal 3
 
-Preview ada di demo.mp4
+async* menandakan asynchronous generator, sehingga dapat menggunakan yield dan yield*
+yield* menandakan stream berterus, Stream.periodic membuat stream dalam interval 1 detik
+
+Soal 5
+listen non blocking, sedangkan await for tidak
--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-
+import 'stream.dart';
 
 void main() {
   runApp(const MyApp());
@@ -28,9 +28,35 @@ class StreamHomePage extends StatefulWidget {
 }
 
 class _StreamHomePageState extends State<StreamHomePage> {
+  Color bgColor = Colors.blueGrey;
+  late ColorStream colorStream;
+
+  void changeColor() async {
+    await for (var eventColor in colorStream.getColors()) {
+      setState(() {
+        bgColor = eventColor;
+      });
+    }
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    colorStream = ColorStream();
+    changeColor();
+  }
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Stream'),
+      ),
+      body: Container(
+        decoration: BoxDecoration(color: bgColor),
+      ),
+    );
   }
+
+
 }
