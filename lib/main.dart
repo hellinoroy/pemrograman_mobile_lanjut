f54249fd9edbbc90050b815264a142c39137d3d2
hellinoroy <marchellinoryo@gmail.com>
2026-06-09 19:25:42 +0700

    W12: Jawaban Soal 1

--- lib/main.dart
+++ lib/main.dart
@@ -1,38 +1,36 @@
 import 'package:flutter/material.dart';
 
-import 'package:go_router/go_router.dart';
-import 'package:uts/pages/homepage.dart';
-import 'package:uts/pages/project.dart';
 
 void main() {
   runApp(const MyApp());
 }
 
-final _router = GoRouter(
-  initialLocation: '/',
-  routes: [
-    GoRoute(
-      path: '/',
-      builder: (context, state) => const Homepage(),
-    ),
-    GoRoute(
-      path: '/project/:name',
-      builder: (context, state) {
-        final name = state.pathParameters['name']!;
-        return Project(name: name);
-      },
-    ),
-  ],
-);
-
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp.router(
-      routerConfig: _router,
+    return MaterialApp(
+        title: '411221221',
+        theme: ThemeData(
+            primarySwatch: Colors.deepPurple,
+        ),
+        home: const StreamHomePage()
     );
+  }
+}
+
+class StreamHomePage extends StatefulWidget {
+    const StreamHomePage({super.key});
+
+  @override
+  State<StatefulWidget> createState() => _StreamHomePageState();
+}
+
+class _StreamHomePageState extends State<StreamHomePage> {
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
   }
 }
