e064c824409e2cca31ddc35b332d878ab43fa478
hellinoroy <marchellinoryo@gmail.com>
2026-06-15 18:55:43 +0700

    W13: Jawaban Soal 2

--- lib/main.dart
+++ lib/main.dart
@@ -8,31 +8,78 @@ void main() {
   runApp(const MyApp());
 }
 
-final _router = GoRouter(
-  initialLocation: '/',
-  routes: [
-    GoRoute(
-      path: '/',
-      builder: (context, state) => const Homepage(),
-    ),
-    GoRoute(
-      path: '/project/:name',
-      builder: (context, state) {
-        final name = state.pathParameters['name']!;
-        return Project(name: name);
-      },
-    ),
-  ],
-);
+// final _router = GoRouter(
+//   initialLocation: '/',
+//   routes: [
+//     GoRoute(
+//       path: '/',
+//       builder: (context, state) => const Homepage(),
+//     ),
+//     GoRoute(
+//       path: '/project/:name',
+//       builder: (context, state) {
+//         final name = state.pathParameters['name']!;
+//         return Project(name: name);
+//       },
+//     ),
+//   ],
+// );
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
   // This widget is the root of your application.
+  // @override
+  // Widget build(BuildContext context) {
+  //   return MaterialApp.router(
+  //     routerConfig: _router,
+  //   );
+  // }
+
   @override
   Widget build(BuildContext context) {
-    return MaterialApp.router(
-      routerConfig: _router,
+    return MaterialApp(
+      title: 'Flutter JSON Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const MyHomePage()
     );
   }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key});
+
+  @override
+  State<StatefulWidget> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  String pizzaString = '';
+
+  @override
+  void initState() {
+    super.initState();
+    readJsonFile();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('JSON'),
+      ),
+      body: Text(pizzaString),
+    );
+  }
+
+  Future readJsonFile() async {
+    String myString = await DefaultAssetBundle.of(context).loadString('assets/pizzalist.json');
+    setState(() {
+      pizzaString = myString;
+    });
+  }
+
+
 }
